0b0ba817827d42e0a63d508bb25b345742b44325
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2018-07-09 13:53:00 +0300

    fix issue with bytes32 parameter

--- jcash/commonutils/eth_contracts.py
+++ jcash/commonutils/eth_contracts.py
@@ -128,7 +128,7 @@ def __transferEth(abi, contract_address, tx_hash, token_address, to_address, val
                          contract_address,
                          ETH_EXCHANGER__ADDRESS,
                          "transferEth",
-                         (tx_hash, w3.toChecksumAddress(to_address), _value_wei),
+                         (w3.toBytes(tx_hash), w3.toChecksumAddress(to_address), _value_wei),
                          ETH_EXCHANGER__PRIVATE_KEY,
                          nonce)
     return _tx_id
@@ -142,7 +142,7 @@ def __transferToken(abi, contract_address, tx_hash, token_address, to_address, v
                          contract_address,
                          ETH_EXCHANGER__ADDRESS,
                          "transferToken",
-                         (tx_hash, w3.toChecksumAddress(token_address), w3.toChecksumAddress(to_address), _value_wei),
+                         (w3.toBytes(tx_hash), w3.toChecksumAddress(token_address), w3.toChecksumAddress(to_address), _value_wei),
                          ETH_EXCHANGER__PRIVATE_KEY,
                          nonce)
     return _tx_id
@@ -156,7 +156,7 @@ def __refundEth(abi, contract_address, tx_hash, token_address, to_address, value
                          contract_address,
                          ETH_EXCHANGER__ADDRESS,
                          "refundEth",
-                         (tx_hash, w3.toChecksumAddress(to_address), _value_wei),
+                         (w3.toBytes(tx_hash), w3.toChecksumAddress(to_address), _value_wei),
                          ETH_EXCHANGER__PRIVATE_KEY,
                          nonce)
     return _tx_id
@@ -170,7 +170,7 @@ def __refundToken(abi, contract_address, tx_hash, token_address, to_address, val
                          contract_address,
                          ETH_EXCHANGER__ADDRESS,
                          "refundToken",
-                         (tx_hash, w3.toChecksumAddress(token_address), w3.toChecksumAddress(to_address), _value_wei),
+                         (w3.toBytes(tx_hash), w3.toChecksumAddress(token_address), w3.toChecksumAddress(to_address), _value_wei),
                          ETH_EXCHANGER__PRIVATE_KEY,
                          nonce)
     return _tx_id
